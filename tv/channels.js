993bb7084d8876787fc44ca9c11431647f473cc4
Moisés Morais <moises.j.morais.lima@gmail.com>
2026-01-01 17:07:25 -0300

    Add files via upload

---
+++ tv/channels.js
@@ -0,0 +1,81 @@
+				/* LISTA DE CANAIS */
+const canais = [
+/* ========== CANAIS ABERTOS ========== */
+  {
+    nome: "TV Cultura",
+    categoria: "Abertos",
+    logo: "https://embedtvonline.com/images/tvcultura.png",
+    url: "https://embedtvonline.com/tvcultura"
+  },
+  {
+    nome: "SBT SP",
+    categoria: "Abertos",
+    logo: "https://embedtvonline.com/images/sbtsp.png",
+    url: "https://embedtvonline.com/sbtsp"
+  },
+  {
+    nome: "Globo SP",
+    categoria: "Abertos",
+    logo: "https://embedtvonline.com/images/globo.webp",
+    url: "https://embedtvonline.com/globosp"
+  },
+  {
+    nome: "Record SP",
+    categoria: "Abertos",
+    logo: "https://embedtvonline.com/images/recordsp.png",
+    url: "https://embedtvonline.com/recordsp"
+  },
+  {
+    nome: "RedeTV SP",
+    categoria: "Abertos",
+    logo: "https://embedtvonline.com/images/redetv.png",
+    url: "https://embedtvonline.com/redetv"
+  },
+  {
+    nome: "Band SP",
+    categoria: "Abertos",
+    logo: "https://embedtvonline.com/images/bandsp.png",
+    url: "https://embedtvonline.com/bandsp"
+  },
+  
+/* ========== CANAIS DE ESPORTE ========== */
+  {
+    nome: "ESPN",
+    categoria: "Esporte",
+    logo: "https://embedtvonline.com/images/espn.png",
+    url: "https://embedtvonline.com/espn"
+  },
+  
+/* ========== CANAIS DE NOTÍCIAS ========== */
+  {
+    nome: "Globo News",
+    categoria: "Notícias",
+    logo: "https://embedtvonline.com/images/globonews.png",
+    url: "https://embedtvonline.com/globonews"
+  },
+  {
+    nome: "Record News",
+    categoria: "Notícias",
+    logo: "https://embedtvonline.com/images/recordnews.png",
+    url: "https://embedtvonline.com/recordnews"
+  },
+  {
+    nome: "Band News",
+    categoria: "Notícias",
+    logo: "https://embedtvonline.com/images/bandnews.png",
+    url: "https://embedtvonline.com/bandnews"
+  },
+  {
+    nome: "Jovem Pan News",
+    categoria: "Notícias",
+    logo: "https://embedtvonline.com/images/jovempannews.png",
+    url: "https://embedtvonline.com/jovempannews"
+  },
+  {
+    nome: "CNN Brasil",
+    categoria: "Notícias",
+    logo: "https://embedtvonline.com/images/cnnbrasil.png",
+    url: "https://embedtvonline.com/cnnbrasil"
+  },
+  
+];
