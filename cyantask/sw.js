247c5922e3298d2042340263796aa85c55e7fabd
Moisés Morais <moises.j.morais.lima@gmail.com>
2026-01-02 22:19:51 -0300

    Create sw.js

---
+++ cyantask/sw.js
@@ -0,0 +1,58 @@
+const CACHE_NAME = 'cyantask-cache-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './tasks.html',
+  './manifest.json',
+  './bottoast.js',
+  './security.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Cyantask: Armazenando arquivos essenciais no cache');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Cyantask: Removendo cache antigo');
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      }).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
