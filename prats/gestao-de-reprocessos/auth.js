d7e74669b9ba00ed93c3f720cf4ca4f64e2782a0
Moisés Morais <moises.j.morais.lima@gmail.com>
2026-01-08 16:22:56 -0300

    Add files via upload

---
+++ prats/gestao-de-reprocessos/auth.js
@@ -0,0 +1,35 @@
+const USUARIOS_PERMITIDOS = {
+	"moises.morais": "32489657",
+	"jhonn.santos": "1910",
+	"mateus.alves": "@prats5000",
+	"carlos.domingos": "@prats5000",
+    "prats": "admin"
+};
+
+const loginForm = document.getElementById('loginForm');
+
+if (loginForm) {
+    loginForm.addEventListener('submit', function(e) {
+        e.preventDefault();
+        
+        const user = document.getElementById('username').value;
+        const pass = document.getElementById('password').value;
+        const erroMsg = document.getElementById('erro');
+
+        if (USUARIOS_PERMITIDOS[user] && USUARIOS_PERMITIDOS[user] === pass) {
+            // Salva o login na sessão (apaga ao fechar o navegador)
+            sessionStorage.setItem('usuarioLogado', user);
+            sessionStorage.setItem('autenticado', 'true');
+            window.location.href = 'rep-nfc.html';
+        } else {
+            erroMsg.style.display = 'block';
+        }
+    });
+}
+
+// Função de Proteção: Adicione esta chamada no topo do seu arquivo rep-nfc.html
+function verificarAutenticacao() {
+    if (sessionStorage.getItem('autenticado') !== 'true') {
+        window.location.href = 'login.html';
+    }
+}
